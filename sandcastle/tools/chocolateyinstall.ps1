6d11294d8ae13615f481e9dadc4f42b5b9ee7ce8
Frederik Carlier <frederik.carlier@quamotion.mobi>
2015-05-27 23:53:56 +0200

    Package SandCastle Tools and Help File Builder

---
+++ sandcastle/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿# stop on all errors
+$ErrorActionPreference = 'Stop';
+
+$packageName = 'sandcastle' # arbitrary name for the package, used in messages
+$registryUninstallerKeyName = 'sandcastle' #ensure this is the value in the registry
+$installerType = 'MSI' #only one of these: exe, msi, msu
+$url = 'https://github.com/EWSoftware/SHFB/releases/download/v2015.5.2.0/SHFBInstaller_v2015.5.2.0.zip' # download url
+$silentArgs = '/quiet'
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$zipFile = Join-Path $toolsDir SHFBInstaller_v2015.5.2.0.zip
+$zipDir = Join-Path $toolsDir SHFBInstaller_v2015.5.2.0
+
+Get-ChocolateyWebFile "$packageName" "$zipFile" "$url" 
+Get-ChocolateyUnzip "$zipFile" "$zipDir"
+
+$file = Join-Path $zipDir "InstallResources\SandcastleHelpFileBuilder.msi"
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$file"
---
+++ sandcastle/tools/chocolateyuninstall.ps1
@@ -0,0 +1,15 @@
+﻿# stop on all errors
+$ErrorActionPreference = 'Stop';
+
+$packageName = 'sandcastle'
+
+try
+{
+	$msiPackageGuid = '{07BAE775-6B29-4D6D-974B-57C5BFA5EF59}'
+	$msiArgs = "/x $msiPackageGuid /qn REBOOT=ReallySuppress"
+
+	Start-ChocolateyProcessAsAdmin "$msiArgs" 'msiexec'
+	Write-ChocolateySuccess $package
+} catch {
+	Write-ChocolateyFailure $packageName "$($_.Exception.Message)"
+}
