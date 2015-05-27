140162ddc7ec792cc909c2950f79f01f37111901
Frederik Carlier <frederik.carlier@quamotion.mobi>
2015-05-28 00:10:20 +0200

    Add Quamotion package

---
+++ quamotion/tools/chocolateyinstall.ps1
@@ -0,0 +1,11 @@
+﻿# stop on all errors
+$ErrorActionPreference = 'Stop';
+
+$packageName = 'quamotion' # arbitrary name for the package, used in messages
+$registryUninstallerKeyName = '{07B0C5A9-BA04-4D86-B3E3-2E7D1BFE13F5}' #ensure this is the value in the registry
+$installerType = 'MSI' #only one of these: exe, msi, msu
+$url = 'http://quamotion.mobi/Product/DownloadQuamotion?user=chocolatey' # download url
+$silentArgs = '/quiet'
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"
---
+++ quamotion/tools/chocolateyuninstall.ps1
@@ -0,0 +1,14 @@
+﻿# stop on all errors
+$ErrorActionPreference = 'Stop';
+$packageName = 'quamotion'
+
+try
+{
+	$msiPackageGuid = '{07B0C5A9-BA04-4D86-B3E3-2E7D1BFE13F5}'
+	$msiArgs = "/x $msiPackageGuid /qn REBOOT=ReallySuppress"
+
+	Start-ChocolateyProcessAsAdmin "$msiArgs" 'msiexec'
+	Write-ChocolateySuccess $package
+} catch {
+	Write-ChocolateyFailure $packageName "$($_.Exception.Message)"
+}
