3214a3d17bb0eb24982f0dc31963e3a06e1190f2
Frederik Carlier <frederik.carlier@quamotion.mobi>
2015-05-27 22:39:50 +0200

    Create Remote TestKit chocolatey package

---
+++ remotetestkit/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿# stop on all errors
+$ErrorActionPreference = 'Stop';
+
+# Variables
+$packageName = 'remotetestkit' # arbitrary name for the package, used in messages
+$registryUninstallerKeyName = '{2DF95E00-F429-4B59-B7D7-F24B82D3272E}_is1' #ensure this is the value in the registry
+$installerType = 'EXE'
+$url = 'https://appkitbox.com/en/wp-content/download/remotetestkit-en.exe' # download url
+$silentArgs = '/VERYSILENT'
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+# main helper functions - these have error handling tucked into them already
+# see https://github.com/chocolatey/choco/wiki/HelpersReference
+# Install an application, will assert administrative rights
+# add additional optional arguments as necessary
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"
---
+++ remotetestkit/tools/chocolateyuninstall.ps1
@@ -0,0 +1,20 @@
+﻿# Stop on all errors
+$ErrorActionPreference = 'Stop';
+
+# Auto Uninstaller should be able to detect and handle registry uninstalls (if it is turned on, it is in preview for 0.9.9).
+$packageName = 'remotetestkit'
+$registryUninstallerKeyName = '{2DF95E00-F429-4B59-B7D7-F24B82D3272E}_is1' #ensure this is the value in the registry
+$installerType = 'EXE'
+$silentArgs = '/VERYSILENT'
+$validExitCodes = @(0)
+
+if ([Environment]::Is64BitProcess)
+{
+	$file = (Get-ItemProperty "hklm:SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\{2DF95E00-F429-4B59-B7D7-F24B82D3272E}_is1").UninstallString
+}
+else
+{
+	$file = (Get-ItemProperty "hklm:SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{2DF95E00-F429-4B59-B7D7-F24B82D3272E}_is1").UninstallString
+}
+
+Uninstall-ChocolateyPackage -PackageName $packageName -FileType $installerType -SilentArgs $silentArgs -validExitCodes $validExitCodes -File $file
