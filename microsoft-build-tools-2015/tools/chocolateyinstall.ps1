86a71fc49f9d1eacb5b412bb9719b7d15b31550e
Frederik Carlier <frederik.carlier@quamotion.mobi>
2015-08-17 10:13:58 +0200

    Create package for Microsoft Build Tools 2015

---
+++ microsoft-build-tools-2015/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$packageArgs = @{
+  packageName   = 'microsoft-build-tools-2015'
+  fileType      = 'EXE' #only one of these: exe, msi, msu
+  url           = 'http://download.microsoft.com/download/E/E/D/EEDF18A8-4AED-4CE0-BEBE-70A83094FC5A/BuildTools_Full.exe'
+  silentArgs    ='/Silent /NoRestart /Log $($env:temp)'
+  validExitCodes = @(
+        0, # success
+        3010 # success, restart required
+    )
+}
+Install-ChocolateyPackage @packageArgs
