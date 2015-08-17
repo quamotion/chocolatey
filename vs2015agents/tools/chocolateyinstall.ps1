f9f573d0d73847804fc785098fc7ea3f5aa00fcd
Frederik Carlier <frederik.carlier@quamotion.mobi>
2015-08-17 10:54:49 +0200

    Add Agents for Visual Studio 2015 Agents

---
+++ vs2015agents/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$packageArgs = @{
+  packageName   = 'vs2015agents'
+  fileType      = 'EXE' #only one of these: exe, msi, msu
+  url           = 'http://download.microsoft.com/download/8/A/F/8AFFDD5A-53D9-46EB-98D7-B61BBCAF0DE6/vstf_testagent.exe'
+  silentArgs    ='/Silent /NoRestart /Log $($env:temp)'
+  validExitCodes = @(
+        0, # success
+        3010 # success, restart required
+    )
+}
+Install-ChocolateyPackage @packageArgs
