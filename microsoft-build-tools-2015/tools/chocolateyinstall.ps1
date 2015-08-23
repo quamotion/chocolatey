4a28c6040a1298dd7571f470d29641bb42288584
Frederik Carlier <frederik.carlier@quamotion.mobi>
2015-08-23 22:16:23 +0200

    Update /log parameter for the VS 2015 installers: provide a file name

--- microsoft-build-tools-2015/tools/chocolateyinstall.ps1
+++ microsoft-build-tools-2015/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName   = 'microsoft-build-tools-2015'
   fileType      = 'EXE' #only one of these: exe, msi, msu
   url           = 'http://download.microsoft.com/download/E/E/D/EEDF18A8-4AED-4CE0-BEBE-70A83094FC5A/BuildTools_Full.exe'
-  silentArgs    ='/Silent /NoRestart /Log $($env:temp)'
+  silentArgs    ='/Silent /NoRestart /Log $($env:temp\microsoft-build-tools-2015.log)'
   validExitCodes = @(
         0, # success
         3010 # success, restart required
--- vs2015agents/tools/chocolateyinstall.ps1
+++ vs2015agents/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName   = 'vs2015agents'
   fileType      = 'EXE' #only one of these: exe, msi, msu
   url           = 'http://download.microsoft.com/download/8/A/F/8AFFDD5A-53D9-46EB-98D7-B61BBCAF0DE6/vstf_testagent.exe'
-  silentArgs    ='/Silent /NoRestart /Log $($env:temp)'
+  silentArgs    ='/Silent /NoRestart /Log $($env:temp\vs2015agents.log)'
   validExitCodes = @(
         0, # success
         3010 # success, restart required
