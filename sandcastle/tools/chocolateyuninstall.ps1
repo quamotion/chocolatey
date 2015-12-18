cf079a126f9d2afffcd6f937ccebc674db794aff
Pascal Berger <pascal.berger@ioz.ch>
2015-12-18 10:08:54 +0100

    Remove deprecated Error / Success Functions

--- sandcastle/tools/chocolateyuninstall.ps1
+++ sandcastle/tools/chocolateyuninstall.ps1
@@ -1,15 +1,7 @@
 ﻿# stop on all errors
 $ErrorActionPreference = 'Stop';
 
-$packageName = 'sandcastle'
+$msiPackageGuid = '{07BAE775-6B29-4D6D-974B-57C5BFA5EF59}'
+$msiArgs = "/x $msiPackageGuid /qn REBOOT=ReallySuppress"
 
-try
-{
-	$msiPackageGuid = '{07BAE775-6B29-4D6D-974B-57C5BFA5EF59}'
-	$msiArgs = "/x $msiPackageGuid /qn REBOOT=ReallySuppress"
-
-	Start-ChocolateyProcessAsAdmin "$msiArgs" 'msiexec'
-	Write-ChocolateySuccess $package
-} catch {
-	Write-ChocolateyFailure $packageName "$($_.Exception.Message)"
-}
+Start-ChocolateyProcessAsAdmin "$msiArgs" 'msiexec'
