7cc83c6c1af019dcefa81610a40bf0a2d395ee55
Frederik Carlier <frederik.carlier@quamotion.mobi>
2018-01-25 22:24:54 +0100

    Remove comments

--- remotetestkit/tools/chocolateyinstall.ps1
+++ remotetestkit/tools/chocolateyinstall.ps1
@@ -1,17 +1,11 @@
-﻿# stop on all errors
-$ErrorActionPreference = 'Stop';
+﻿$ErrorActionPreference = 'Stop';
 
-# Variables
-$packageName = 'remotetestkit' # arbitrary name for the package, used in messages
-$registryUninstallerKeyName = '{2DF95E00-F429-4B59-B7D7-F24B82D3272E}_is1' #ensure this is the value in the registry
+$packageName = 'remotetestkit'
+$registryUninstallerKeyName = '{2DF95E00-F429-4B59-B7D7-F24B82D3272E}_is1'
 $installerType = 'EXE'
-$url = 'https://appkitbox.com/en/wp-content/download/remotetestkit-en.exe' # download url
+$url = 'https://appkitbox.com/en/wp-content/download/remotetestkit-en.exe'
 $silentArgs = '/VERYSILENT'
-$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+$validExitCodes = @(0)
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-# main helper functions - these have error handling tucked into them already
-# see https://github.com/chocolatey/choco/wiki/HelpersReference
-# Install an application, will assert administrative rights
-# add additional optional arguments as necessary
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"
--- remotetestkit/tools/chocolateyuninstall.ps1
+++ remotetestkit/tools/chocolateyuninstall.ps1
@@ -1,9 +1,7 @@
-﻿# Stop on all errors
-$ErrorActionPreference = 'Stop';
+﻿$ErrorActionPreference = 'Stop';
 
-# Auto Uninstaller should be able to detect and handle registry uninstalls (if it is turned on, it is in preview for 0.9.9).
 $packageName = 'remotetestkit'
-$registryUninstallerKeyName = '{2DF95E00-F429-4B59-B7D7-F24B82D3272E}_is1' #ensure this is the value in the registry
+$registryUninstallerKeyName = '{2DF95E00-F429-4B59-B7D7-F24B82D3272E}_is1'
 $installerType = 'EXE'
 $silentArgs = '/VERYSILENT'
 $validExitCodes = @(0)
