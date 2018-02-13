ff35966b23aec2aaed43dfb71cc0fc52314e9ae7
Frederik Carlier <frederik.carlier@quamotion.mobi>
2018-02-13 14:00:35 +0100

    Chocolatey review feedback

--- remotetestkit/tools/chocolateyinstall.ps1
+++ remotetestkit/tools/chocolateyinstall.ps1
@@ -1,11 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageName = 'remotetestkit'
-$registryUninstallerKeyName = '{2DF95E00-F429-4B59-B7D7-F24B82D3272E}_is1'
 $installerType = 'EXE'
-$url = 'https://appkitbox.com/en/wp-content/download/remotetestkit-en.exe'
 $silentArgs = '/VERYSILENT'
-$validExitCodes = @(0)
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://appkitbox.com/en/wp-content/download/remotetestkit-en.exe'
 
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"
